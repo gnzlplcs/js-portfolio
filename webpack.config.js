ede087288098b2d960416fb3f32141a84228fe4c
gnzlplcs <palaciosgm@outlook.com>
2022-02-10 15:56:17 -0500

    variables de entorno

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const DotEnv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -64,7 +65,8 @@ module.exports = {
           to: "assets/images"
         }
       ]
-    })
+    }),
+    new DotEnv(),
   ],
   optimization: {
     minimize: true,
