9aef308df111cbadabaf58fd421f05522bd697e6
gnzlplcs <palaciosgm@outlook.com>
2022-02-28 00:02:44 -0500

    :sparkles: webpack bundle analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.js',
@@ -66,6 +67,7 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     static: {
